3b0ae96c7ee44c88f47640de31f9394295f3be94
Yasin Ozel <yasin.ozel@modanisa.com>
2020-12-12 14:01:06 +0300

    Revert "Remove cache"
    This reverts commit a81279eddafaf97d1a8ddc20ddd0a200128dd4a5.

--- gatherer/gatherer.go
+++ gatherer/gatherer.go
@@ -1,6 +1,17 @@
 package gatherer
 
 import (
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/url"
+	"os"
+	"path"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -10,9 +21,97 @@ type Gatherer struct {
 	descriptors map[string]func(options map[string]string) ([]RawObjectInterface, error)
 }
 
+func (g *Gatherer) getCachePath(cacheKey string, options map[string]string) string {
+	keys := make([]string, 0, len(options))
+	for k := range options {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	h := sha256.New()
+	for _, k := range keys {
+		k = url.QueryEscape(k)
+		v := url.QueryEscape(options[k])
+		_, err := h.Write([]byte(fmt.Sprintf("%s:%s;", k, v)))
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	key := fmt.Sprintf("%s-%s", strings.Replace(cacheKey, "/", ".", -1), fmt.Sprintf("%x", h.Sum(nil)))
+	return path.Join(g.cachePath, key)
+}
+
+func (g *Gatherer) setCache(cacheFilePath string, objects []RawObjectInterface, ttl time.Duration) error {
+	rawObjects := []RawObject{}
+	for _, obj := range objects {
+		rawObjects = append(rawObjects, obj.Copy())
+	}
+	b, err := json.Marshal(rawObjects)
+	if err != nil {
+		return err // TODO: wrap error
+	}
+	err = ioutil.WriteFile(fmt.Sprintf("%s-%d", cacheFilePath, time.Now().Unix()+int64(ttl.Seconds())), b, 0644)
+	if err != nil {
+		return err // TODO: wrap error
+	}
+	return nil
+}
+
+func (g *Gatherer) getCache(cacheFilePath string) ([]RawObject, error) {
+
+	files, err := filepath.Glob(fmt.Sprintf("%s-*", cacheFilePath))
+	if err != nil {
+		return nil, err // TODO: wrap error
+	}
+	if len(files) == 0 {
+		return nil, nil
+	}
+	sort.Strings(files)
+	cacheFilePath = files[len(files)-1]
+
+	cs := strings.Split(cacheFilePath, "-")
+
+	timestamp, err := strconv.Atoi(cs[len(cs)-1])
+	if err != nil {
+		return nil, err // TODO: wrap error
+	}
+
+	if int64(timestamp) < time.Now().Unix() {
+		return nil, nil
+	}
+
+	b, err := ioutil.ReadFile(cacheFilePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, nil
+		}
+		return nil, err // TODO: wrap error
+	}
+
+	var d []RawObject
+	err = json.Unmarshal(b, &d)
+	if err != nil {
+		return nil, err // TODO: wrap error
+	}
+	return d, nil
+}
+
 // Gather returns Description for given name and options with cache
-func (g *Gatherer) Gather(key string, options map[string]string, ttl time.Duration) ([]RawObjectInterface, error) {
-	r, err := g.descriptors[key](options)
+func (g *Gatherer) Gather(key string, options map[string]string, ttl time.Duration) ([]RawObject, error) {
+	cp := g.getCachePath(key, options)
+	r, err := g.getCache(cp)
+	if err != nil {
+		return nil, err // TODO: wrap error
+	}
+	if r != nil {
+		return r, nil
+	}
+
+	r2, err := g.descriptors[key](options)
+	if err != nil {
+		return nil, err // TODO: wrap error
+	}
+	err = g.setCache(cp, r2, ttl)
 	if err != nil {
 		return nil, err // TODO: wrap error
 	}
